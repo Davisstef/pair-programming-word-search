b8bdc88f23bce6d2d0deeebe3b3a8a3af7cf6c1f
vagrant <stefan.davis@mail.utoronto.ca>
2021-11-05 05:26:21 +0000

    Wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const wordSearch = (letters, word) => {
+  if (letters.length < 1 || word.length < 1)
+  return false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin)  {
+      if (l.includes(word)) return true
+  }
+  newLetters = transpose(letters);
+  const vertJoin = newLetters.map(vs => vs.join(''));
+  for (x of vertJoin)  {
+      if (x.includes(word)) return true
+ }
+ return false;
 }
-
+const transpose = function(matrix) {
+  let newMat = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMat.push([]);
+  }
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      newMat[col][row] = matrix[row][col];
+    }
+  }
+  return newMat;
+};
 module.exports = wordSearch
